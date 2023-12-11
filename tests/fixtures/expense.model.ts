02f04a0a5acad73fc8dd4aed54449d009d98f604
MailtonOn <mailton.oliveira@onfly.com.br>
2023-12-11 00:53:12 -0300

    first commit

---
+++ tests/fixtures/expense.model.ts
@@ -0,0 +1,12 @@
+export interface ExpenseModel {
+    category: string
+    value: string
+    description: string
+    establishment: string
+    report: string
+    pointA: string
+    selectPointA: string
+    pointB: string
+    selectPointB: string
+    km: string
+}
